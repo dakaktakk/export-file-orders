bd9afa9fd65e0e8e2b3225c518f010432ab87b97
dakaktak <dakaktakk@gmail.com>
2026-07-02 14:48:20 +0300

    убрал IOException

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         //Path path = Paths.get("src/main/resources/files/orders/discount_day.txt");
         Path path = Paths.get("src/main/resources/files/orders/discount_day_without_ext");
